d6fc81aaaa77b3450312a1ad8c37cb1e18988d1b
SShubhamKSahu <shubham.official571@gmail.com>
2025-08-21 11:33:45 +0530

    initial commit

---
+++ projects/password-generator/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Password generator</h1>
+      <h2 className='text-center text-2xl text-white'>Hi</h2>
+    </>
+  )
+}
+
+export default App
