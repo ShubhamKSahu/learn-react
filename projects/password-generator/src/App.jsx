0676a5c882b00060ecf88b30b3e99beec961be65
SShubhamKSahu <shubham.official571@gmail.com>
2025-08-24 22:00:08 +0530

    initial commit

--- projects/password-generator/src/App.jsx
+++ projects/password-generator/src/App.jsx
@@ -1,14 +1,31 @@
 
 import './App.css'
-
+import { useState, useCallback } from 'react';
 function App() {
- 
+ const [length, setLength] = useState(8);
+ const [numAllowed, setNumAllowed] = useState(false);
+ const [charAllowed, setCharAllowed] = useState(false);
+ const [password, setPassword] = useState('');
+
+ const passwordGenerator = useCallback(()=>{
+const pswd = '';
+let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+if(numAllowed){
+  str = str+ '0123456789'
+}
+if(charAllowed){
+  str = str+ '~!@#$%^&*(){}?<>'
+}
+
+ }, [length, numAllowed, charAllowed])
   return (
-    <>
-      <h1 className='text-4xl text-center text-white'>Password generator</h1>
-      <h2 className='text-center text-2xl text-white'>Hi</h2>
-    </>
-  )
+    <div className="text-white password-container">
+      <div className="password-field">
+        <input type="text"></input>
+        <button>copy</button>
+      </div>
+    </div>
+  );
 }
 
 export default App
