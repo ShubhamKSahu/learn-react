1603fd8c4079b86058bc4e55338897267308834f
SShubhamKSahu <shubham.official571@gmail.com>
2025-08-28 18:07:09 +0530

    usage of NavLink instead of Link

--- projects/02-starting-project/src/components/MainPageNavigation.jsx
+++ projects/02-starting-project/src/components/MainPageNavigation.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from './mainPageNavigation.module.css'
 
 function MainPageNavigation(){
   return <header className={styles.header}>
     <nav>
-      <ul style={styles.li}>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/products'>Products</Link></li>
+      <ul className={styles.list}>
+        <li><NavLink to='/' className={({ isActive }) => isActive? styles.active : undefined}> Home</NavLink></li>
+        <li ><NavLink to='/products' className={({ isActive }) => isActive? styles.active : undefined}>Products</NavLink></li>
       </ul>
     </nav>
   </header>;
