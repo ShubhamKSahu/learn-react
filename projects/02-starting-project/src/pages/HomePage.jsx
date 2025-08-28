c7d52ccae77b203eff2794d49eafb5f499198183
SShubhamKSahu <shubham.official571@gmail.com>
2025-08-28 12:24:14 +0530

    navigating between pages using Link.

--- projects/02-starting-project/src/pages/HomePage.jsx
+++ projects/02-starting-project/src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 function HomePage(){
-    return <h2>My Home Page</h2>
+    return <>
+    <h2>My Home Page</h2>
+    {/* <h5>Go to <a href="/products">products page.</a></h5> */}
+    <h5>Go to <Link to="/products">products page.</Link></h5>
+    </>
 }
 
 export default HomePage;
