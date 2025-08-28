53f43facc137b3ff375a7db67d9f557472f8c210
SShubhamKSahu <shubham.official571@gmail.com>
2025-08-28 11:35:09 +0530

    added a second route.

--- projects/02-starting-project/src/App.js
+++ projects/02-starting-project/src/App.js
@@ -1,9 +1,11 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 const router = createBrowserRouter([
-  { path : '/', element: <HomePage/>}
+  { path : '/', element: <HomePage/>},
+  { path: '/products', element: <ProductPage/>}
 ])
 function App() {
   return <RouterProvider router={router}/>
---
+++ projects/02-starting-project/src/pages/ProductPage.jsx
@@ -0,0 +1,5 @@
+const ProductPage = () =>{
+    return <h2>This is a Paroduct page.</h2>
+}
+
+export default ProductPage;
